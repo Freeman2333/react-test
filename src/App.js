87c42ad06cec1e05c9b6d8f3427e0542847c0b5f
freeman2333 <freeman23@bigmir.net>
2020-12-31 07:49:45 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -4,20 +4,16 @@ import BtnComponent from './Components/BtnComponent';
 import './App.css';
 
 function App() {
-  const [time, setTime] = useState({ms:0, s:0, m:0, h:0});
+  const [time, setTime] = useState({s:0, m:0, h:0});
   const [interv, setInterv] = useState();
-  const [status, setStatus] = useState(0);
-  // Not started = 0
-  // started = 1
-  // stopped = 2
+  const [startBtnOn, setStartBtnOn] = useState(true);
 
   const start = () => {
-    run();
-    setStatus(1);
-    setInterv(setInterval(run, 10));
+    clearInterval(interv);
+    setInterv(setInterval(run, 1000));
   };
 
-  var updatedMs = time.ms, updatedS = time.s, updatedM = time.m, updatedH = time.h;
+  let updatedS = time.s, updatedM = time.m, updatedH = time.h;
 
   const run = () => {
     if(updatedM === 60){
@@ -28,34 +24,37 @@ function App() {
       updatedM++;
       updatedS = 0;
     }
-    if(updatedMs === 100){
-      updatedS++;
-      updatedMs = 0;
-    }
-    updatedMs++;
-    return setTime({ms:updatedMs, s:updatedS, m:updatedM, h:updatedH});
+    updatedS++;
+    return setTime({ s:updatedS, m:updatedM, h:updatedH});
   };
 
   const stop = () => {
     clearInterval(interv);
-    setStatus(2);
   };
 
   const reset = () => {
     clearInterval(interv);
-    setStatus(0);
-    setTime({ms:0, s:0, m:0, h:0})
+    setInterv('')
+    setTime({s:0, m:0, h:0})
   };
 
   const resume = () => start();
 
+  const startBtnClick = () => {
+    if (interv) {
+      reset()
+    }
+    if (!interv) {
+      start()
+    }
+  }
 
   return (
     <div className="main-section">
      <div className="clock-holder">
           <div className="stopwatch">
                <DisplayComponent time={time}/>
-               <BtnComponent status={status} resume={resume} reset={reset} stop={stop} start={start}/>
+               <BtnComponent  resume={resume} reset={reset} stop={stop} start={startBtnClick}/>
           </div>
      </div>
     </div>
